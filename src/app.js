6266996535fe8c7d783663dcf9187d58441a0468
Jovita <132444930+EmperorJho@users.noreply.github.com>
2023-06-02 16:51:19 +0100

    Showing the current temperature, wind-speed humidity and description

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let units = "metric";
+let apiKey = "dd0a9897597d72cf07bb7f69d232f1da";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
